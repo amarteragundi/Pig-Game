1b2e540b98c05deb9ce989cd18e73f61f60a7f37
amarteragundi <amarteragundi@gmail.com>
2019-04-03 14:14:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
-GAME RULES:
+GAME RULES: Read it carefully
 
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
